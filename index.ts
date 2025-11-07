019719a4b7fd445cc48c6d2b3ebf261427a87d2b
Rafael Mendes <joserafaelmb@gmail.com>
2025-11-07 14:27:03 +0000

    query values added for one id

--- index.ts
+++ index.ts
@@ -8,8 +8,19 @@ import db from "./_db"
 const resolvers = {
     Query: {
         games: () => db.games,
+        game: (_: any, args: { id: string }) => {
+            return db.games.find(game => game.id === args.id)
+        },
+
         authors: () => db.authors,
+        author: (_: any, args: { id: string }) => {
+            return db.authors.find(author => author.id === args.id)
+        },
+
         reviews: () => db.reviews,
+        review(_: any, args: { id: string }) {
+            return db.reviews.find(review => review.id === args.id)
+        }
     }
 };
 // server setup
